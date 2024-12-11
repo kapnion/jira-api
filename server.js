284e32dd3798a14f754ad35d1ceca5d755391a52
Karsten Beuerle <karsten.beuerle@edoc.de>
2024-12-11 02:07:35 +0100

    add gitignore

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.post('/generate', async (req, res) => {
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 2222;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
